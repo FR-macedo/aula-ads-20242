16f0b5c9e64a79eae9aa97155cf11677a7b8169f
FR-macedo <macedo270701@gmail.com>
2024-10-28 20:12:07 -0300

    testando a branch test

---
+++ src/index.js
@@ -0,0 +1,6 @@
+const readline = require('readline-sync');
+
+let mensagem = readline.prompt("Escreve um a mensagem ai, que ela vai ser impressa no terminal.")
+
+console.log("Eis ai tua mensagem:" + mensagem);
+
